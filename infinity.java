b6279be2167fcaba8bffbb590157c4bf156df18a
vigneshkumarme47 <vigneshkumarme47@gmail.com>
2017-06-16 10:05:47 +0530

    Create infinity.java

---
+++ infinity.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class excep
+  {
+    public static void  main(String args[])
+     {
+      // try
+       {
+        int a,b;
+          b=10/0;
+       }
+     //  catch(ArithmeticException e)
+       {
+    //      System.out.println("Exception thrown :"+e);
+       }
+    //   System.out.println("Out of the block");
+     }
+  }
